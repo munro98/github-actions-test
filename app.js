148c59d88a994d262da61039bf46fee804d2d9ee
Nigel Munro <munro98@gmail.com>
2022-07-12 22:35:04 +1200

    small change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("<h1> hello world secret:" + process.env.MY_SECRET + "</h1>");
+    res.send("<h1> hello world secret :" + process.env.MY_SECRET + "</h1>");
 });
 
 app.get('/products', (req, res) => {
