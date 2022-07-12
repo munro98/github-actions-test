b721a20d1d3f72d0795058497801e33371e6e9f5
Nigel Munro <munro98@gmail.com>
2022-07-12 22:16:20 +1200

    test out env

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("<h1> hello world</h1>");
+    res.send("<h1> hello world</h1> secret is " + process.env.MY_SECRET);
 });
 
 app.get('/products', (req, res) => {
